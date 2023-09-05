e06be029d82d96e85e86a7a6027a726488cedaaa
hyunsu00 <hyun00@hancom.com>
2023-09-05 23:27:18 +0900

    업데이트

---
+++ mkdir.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+documents_path="$HOME/HDVDocuments"
+if [ ! -d "$documents_path" ]; then
+    echo "directory \"$documents_path\" is not exists"
+    mkdir -p "$documents_path"
+else
+    echo "directory \"$documents_path\" is exists"
+fi
